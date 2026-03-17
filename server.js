94b2f8a8c98119e3ed26af965e99af22c8f151b0
KeariGaming <keari0010@gmail.com>
2026-03-17 11:32:34 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let players = [];
+
+// Register player
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+
+    if (username && !players.includes(username)) {
+        players.push(username);
+        console.log("Registered:", username);
+    }
+
+    res.send("OK");
+});
+
+// Get all players
+app.get("/players", (req, res) => {
+    res.json(players);
+});
+
+// Flash crossdomain
+app.get("/crossdomain.xml", (req, res) => {
+    res.type("application/xml");
+    res.send(`<?xml version="1.0"?>
+<cross-domain-policy>
+   <allow-access-from domain="*" />
+</cross-domain-policy>`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Server running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Running on port", PORT);
+});
