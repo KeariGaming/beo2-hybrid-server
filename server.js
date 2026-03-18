5f047b1e3c01b690cf1b3fc800d9c521afe1ccb3
KeariGaming <keari0010@gmail.com>
2026-03-17 20:57:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const { MongoClient } = require("mongodb");
 const app = express();
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     methods: ["GET", "POST", "OPTIONS"]
 }));
 app.use(express.json());
